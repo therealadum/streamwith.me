aa6b8f4780cf768f3e481671a3b5ffa6d08a8aa5
Adam Shurson <shurson.adam@gmail.com>
2022-02-27 19:32:31 -0600

    wip

--- _services/api/src/main.ts
+++ _services/api/src/main.ts
@@ -1,26 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const NodeMediaServer = require('node-media-server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
-
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60,
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*',
-  },
-};
-
-var nms = new NodeMediaServer(config);
-nms.run();
--- _services/api/src/stream/stream.service.ts
+++ _services/api/src/stream/stream.service.ts
@@ -1,5 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { PubSubService } from '../_pubsub/pubsub.service';
 
@@ -8,11 +9,14 @@ export class StreamService {
   constructor(
     private pubSubService: PubSubService,
     private httpService: HttpService,
+    private configService: ConfigService,
   ) {}
 
   async list() {
     const request = await firstValueFrom(
-      this.httpService.get('http://localhost:8000/api/streams'),
+      this.httpService.get(
+        `https://${this.configService.get('LIVE_URL')}/api/streams`,
+      ),
     );
     const streamKeys = Object.keys(request.data.live);
     return streamKeys.filter((id) => id !== 'final').map((id) => ({ id }));
